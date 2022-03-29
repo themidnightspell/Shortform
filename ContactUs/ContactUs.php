daeaa420bc73bfd1c6b8ed69d3f5e9580c056ae8
themidnightspell <47506938+themidnightspell@users.noreply.github.com>
2022-03-28 22:34:57 -0500

    Add files via upload

---
+++ ContactUs/ContactUs.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+   <title>ENTER TITLE HERE</title>
+   <meta charset="UTF-8">
+   <meta name="ContactUs" content="Contact">
+   <meta name="Miu Nakata" content="Contact">
+
+      <link rel = "stylesheet" type = "text/css" href = "./ContactUs.css"/>
+
+</head> 
+
+<body>
+    
+     <?php include("../Header/Header.php" ?>
+
+    <tr>
+        <td>
+    
+
+    
+    <h3>Contact Us</h3>
+    
+            <h1><br>Our Email: <br>
+                <a href="mailto:abaali@utexas.edu"><i>Aram Baali</i></a><br>
+                <a href="mailto:nakatmiu@utexas.edu"><i>Miu Nakata</i></a><br>
+                <a href="mailto:alyssalin@utexas.edu"><i>Alyssa Lin</i></a><br>
+                <a href="mailto:sebastiangranhold@utexas.edu"><i>Sebastian Granhold</i></a><br>
+                
+                
+                
+
+    
+    
+                
+    <div class="form">
+    <form>
+        
+    <p>
+      <label> Your Name: <input name = "name" type = "text" size = "30" /> </label>
+   </p>
+
+   <p>
+      <label> Email Address: <input name = "email" type = "text" size = "30" /> </label>
+   </p>
+    <p>
+      <label> Phone Number: <input name = "phone" type = "text" size = "30" /> </label>
+   </p>
+
+
+
+   <p>
+      <label> Additional Comments: <br />
+         <textarea name = "comments" rows = "4" cols = "36"> Enter your comments here: </textarea>
+      </label>
+   </p>
+
+  
+        
+        <td class="buttons">
+   <p>
+      <input type = "submit" value = "Enter" />
+      <input type = "reset" value = "Reset" />
+   </p>
+
+    
+        
+           
+        
+
+</form>
+    </div>
+    <h1><br>
+        <a href="https://spring-2022.cs.utexas.edu/cs329e-bulko/nakatmiu/Shortform/ShortForm-main/Homepage/Body.php"><i>Homepage</i></a></h1>
+    
+    
+</body>
+</html>

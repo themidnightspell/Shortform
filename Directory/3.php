40fa2da8c67e77c159868cef1f6007db59f5775d
alyssajlin <alyssal3@cs.utexas.edu>
2022-05-04 01:11:26 -0500

    update page

--- Directory/3.php
+++ Directory/3.php
@@ -15,6 +15,8 @@
 </td>
 </table>
 
+<p>
+Under a landscape direction and a hypnotic Jazz track, we listen to the people from Toyah, Texas air their concerns about small-town life in the endangered and diverse Chihuahuan desert. Approaching the modern gaze of (9:16) aspect-ratio, we momentarily lapse into a unique façade of life in the desertic south. </p>
 <h3>Cast and Crew:</h3>
 <table>
     <tr>
